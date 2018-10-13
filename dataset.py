00c4869d2e0ebec23c8b4e1c05749d9bb7a1cdf3
Naitian Zhou <naitian@naitian.org>
2018-10-13 13:12:56 -0400

    Add Flag to Load All Data; Add Preprocessing

--- dataset.py
+++ dataset.py
@@ -2,13 +2,18 @@ import glob
 import pandas as pd
 
 
-def get_pandas_df():
+def get_pandas_df(all_csv=False):
     """Returns dataframe with all files
     :returns: TODO
 
+    NOTE: these return dataframes that look different
+    to each other. so. yeah.
     """
+    if all_csv:
+        frame = pd.read_csv('./data/all.csv', encoding="ISO-8859-1", header=0)
+        return frame
     path = r'./data'  # use your path
-    allFiles = glob.glob(path + "/*.csv")
+    allFiles = glob.glob(path + "/*_facebook_statuses.csv")
     frame = pd.DataFrame()
     list_ = []
     for csv_file in allFiles:
@@ -16,3 +21,15 @@ def get_pandas_df():
         list_.append(df)
     frame = pd.concat(list_)
     return frame
+
+
+def preprocess(df, cat=False):
+    res = df.dropna(subset=['status_message'])
+    res = res.drop_duplicates(subset=['status_id'])
+    res['link_name'] = res['link_name'].fillna('')
+    if cat:
+        res['🙀'] = res['status_message'] + ' ' + res['link_name']
+    res['message'] = res['status_message'] + ' ' + res['link_name']
+    res = res[res['status_message'].str.len() < 1000]
+    res = res[res['num_reactions'] - res['num_likes'] > 11]
+    return res
